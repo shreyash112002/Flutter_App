91d35b148f666968d9b306bc6e5f952dfc77e917
shreyash112002 <shreyashypatil1122002@gmail.com>
2023-04-22 13:19:32 +0530

    splash

--- lib/pages/splashScreen.dart
+++ lib/pages/splashScreen.dart
@@ -1,52 +1,33 @@
-import 'dart:async';  
-import 'package:flutter/material.dart';  
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutterapp/main.dart';
+
+import 'homescreen.dart';  
   
-void main() { runApp(MyApp());}  
-  
-class MyApp extends StatelessWidget {  
-  @override  
-  Widget build(BuildContext context) {  
-    return MaterialApp(  
-      home: MyHomePage(),  
-      debugShowCheckedModeBanner: false,  
-    );  
-  }  
-}  
-  
-class MyHomePage extends StatefulWidget {  
-  @override  
-  SplashScreenState createState() => SplashScreenState();  
-}  
-class SplashScreenState extends State<MyHomePage> {  
-  @override  
-  void initState() {  
-    super.initState();  
-    Timer(Duration(seconds: 5),  
-            ()=>Navigator.pushReplacement(context,  
-            MaterialPageRoute(builder:  
-                (context) => HomeScreen()  
-            )  
-         )  
-    );  
-  }  
-  @override  
-  Widget build(BuildContext context) {  
-    return Container(  
-        color: Colors.yellow,  
-        child:FlutterLogo(size:MediaQuery.of(context).size.height)  
-    );  
-  }  
-}  
-class HomeScreen extends StatelessWidget {  
-  @override  
-  Widget build(BuildContext context) {  
-    return Scaffold(  
-      appBar: AppBar(title:Text("Splash Screen Example")),  
-      body: Center(  
-          child:Text("Welcome to Home Page",  
-              style: TextStyle( color: Colors.black, fontSize: 30)  
-          )  
-      ),  
-    );  
-  }  
-}  
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 5),(){
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => HomePage(),));
+    });
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Image.asset("assets/images/parking_logo.png")]),)
+    );
+  }
+}
+
+
