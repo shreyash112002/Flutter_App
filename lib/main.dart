220968f308d9cc1f55120b3cfc8b6156fb326d39
RUGVEDLANGHI17 <rugvedlanghi18@gmail.com>
2023-04-23 11:30:07 +0530

    unnessarylib

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/homescreen.dart';
 import 'package:flutterapp/splashScreen.dart';
-import 'package:flutterapp/utils/mytheme.dart';
 import 'splashScreen.dart';
 import 'SigninScreen.dart';
 import 'homescreen.dart';
