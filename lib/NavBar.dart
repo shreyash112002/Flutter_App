4127af80863b214e60a0dfb6f77ddf7376e5737d
RUGVEDLANGHI17 <rugvedlanghi18@gmail.com>
2023-04-25 20:56:28 +0530

    firestoreconnected

--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
@@ -10,8 +12,86 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
+  User? user;
+  String uid = '';
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      uid = user!.uid;
+    }
+  }
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserData() async {
+    final userRef = FirebaseFirestore.instance.collection('users').doc(uid);
+    final userData = await userRef.get();
+    return userData;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return FutureBuilder(
+      future: getUserData(),
+      builder: (context,
+          AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasData) {
+          final data = snapshot.data?.data();
+          if (data != null) {
+            final userName = data['UserName'] ?? '';
+            final userEmail = user?.email ?? '';
+            final userAvatarText = data['name']?[0] ?? '';
+            return Drawer(
+              child: Column(
+                children: [
+                  UserAccountsDrawerHeader(
+                    accountName: Text(userName),
+                    accountEmail: Text(userEmail),
+                    currentAccountPicture: CircleAvatar(
+                      child: Text(userAvatarText),
+                    ),
+                  ),
+                  // Other Drawer items
+                ],
+              ),
+            );
+          } else {
+            return Drawer(
+              child: Column(
+                children: [
+                  UserAccountsDrawerHeader(
+                    accountName: Text(''),
+                    accountEmail: Text(''),
+                    currentAccountPicture: CircleAvatar(),
+                  ),
+                  // Other Drawer items
+                ],
+              ),
+            );
+          }
+        } else {
+          return Drawer(
+            child: Column(
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text(''),
+                  accountEmail: Text(''),
+                  currentAccountPicture: CircleAvatar(),
+                ),
+                ListTile(
+                  leading: Icon(Icons.info),
+                  title: Text("About us"),
+                  onTap: () {
+                    Navigator.of(context).pushNamed("/about");
+                  },
+                )
+              ],
+            ),
+          );
+        }
+      },
+    );
   }
 }
--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -9,12 +9,14 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shimmer/shimmer.dart';
 
-String stringResponse="";
-Map mapResponse={'data'} as Map;
-Map dataResponse={'data'} as Map;
-List listResponse=['data'];
+String stringResponse = "";
+Map mapResponse = {'data'} as Map;
+Map dataResponse = {'data'} as Map;
+List listResponse = ['data'];
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+  
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -34,190 +36,193 @@ class _HomeScreenState extends State<HomeScreen> {
   //     });
   //   }
   // }
-  final Url="https://script.googleusercontent.com/macros/echo?user_content_key=Ff4PESC-iXtVr9OsRg5R9d1pNDQi9pJMLJ8DO0RE6mgc2i4mN9kSvbTmVzVREB9urZWFTJxJzrewpOuxQTNZqGvuTngthLHtm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPveZDlJ_AsjVJCY6axLhPTyT107DOA6BeV8lpw_FsjPFjpp8noYC4ljtLZPJIzijjJM0SxjCYprq9OmLICGGxEjzvIRtMuI3tz9Jw9Md8uu&lib=MxUoWSxTNx4Hk4n3l8SvlMA9o7N4CQEWp";
+  final Url =
+      "https://script.googleusercontent.com/macros/echo?user_content_key=Ff4PESC-iXtVr9OsRg5R9d1pNDQi9pJMLJ8DO0RE6mgc2i4mN9kSvbTmVzVREB9urZWFTJxJzrewpOuxQTNZqGvuTngthLHtm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPveZDlJ_AsjVJCY6axLhPTyT107DOA6BeV8lpw_FsjPFjpp8noYC4ljtLZPJIzijjJM0SxjCYprq9OmLICGGxEjzvIRtMuI3tz9Jw9Md8uu&lib=MxUoWSxTNx4Hk4n3l8SvlMA9o7N4CQEWp";
   List<Data> _dataList = [];
   bool _isLoading = true;
   Future<void> _getData() async {
-  setState(() {
-    _isLoading = true;
-  });
-  final response = await http.get(Uri.parse(Url));
-  if (response.statusCode == 200) {
     setState(() {
-      _dataList = List<Data>.from(
-          json.decode(response.body)['data'].map((x) => Data.fromJson(x)));
-      _isLoading = false;
+      _isLoading = true;
     });
-  } else {
-    throw Exception('Failed to fetch data');
+    final response = await http.get(Uri.parse(Url));
+    if (response.statusCode == 200) {
+      setState(() {
+        _dataList = List<Data>.from(
+            json.decode(response.body)['data'].map((x) => Data.fromJson(x)));
+        _isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to fetch data');
+    }
   }
-}
 
   @override
   void initState() {
-   _getData();
+    _getData();
     super.initState();
   }
- 
+
   @override
   Widget _buildShimmerEffect() {
-  return Shimmer.fromColors(
-    baseColor: Colors.grey[300]!,
-    highlightColor: Colors.grey[100]!,
-    child: ListView.builder(
-      itemCount: 12,
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            leading: CircleAvatar(
-              radius: 30,
-              backgroundColor: Colors.grey,
-            ),
-            title: SizedBox(
-              height: 20,
-              child: Container(
-                color: Colors.grey,
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      child: ListView.builder(
+        itemCount: 12,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.grey,
+              ),
+              title: SizedBox(
+                height: 20,
+                child: Container(
+                  color: Colors.grey,
+                ),
+              ),
+              subtitle: SizedBox(
+                height: 16,
+                child: Container(
+                  color: Colors.grey,
+                ),
               ),
             ),
-            subtitle: SizedBox(
-              height: 16,
-              child: Container(
-                color: Colors.grey,
-              ),
-            ),
-          ),
-        );
-      },
-    ),
-  );
-}
+          );
+        },
+      ),
+    );
+  }
 
   Widget build(BuildContext context) {
-   return Scaffold(
-        drawer: NavBar(),
-        appBar: AppBar(
-          title: Text('Data Cards'),
-        ),
-        body: _isLoading
-  ? _buildShimmerEffect()
-  : ListView.builder(
-      itemCount: _dataList.length,
-      itemBuilder: (context, index) {
-        final data = _dataList[index];
-        return Card(
-  elevation: 4,
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(10),
-  ),
-  child: Padding(
-    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Date and Time',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          ),
-        ),
-        SizedBox(height: 8),
-        Text(
-          '${data.dateAndTime}',
-          style: TextStyle(
-            fontSize: 14,
-            color: Colors.grey[600],
-          ),
-        ),
-        SizedBox(height: 16),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Total',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[600],
-              ),
-            ),
-            Text(
-              '${data.total}',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 8),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Occupied',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[600],
-              ),
-            ),
-            Text(
-              '${data.occupied}',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
-        ),
-      ],
-    ),
-  ),
-);
+    return Scaffold(
+      drawer: NavBar(),
+      appBar: AppBar(
+        title: Text('Data Cards'),
+      ),
+      body: _isLoading
+          ? _buildShimmerEffect()
+          : ListView.builder(
+              itemCount: _dataList.length,
+              itemBuilder: (context, index) {
+                final data = _dataList[index];
+                return Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Date and Time',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          '${data.dateAndTime}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                            Text(
+                              '${data.total}',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[800],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Occupied',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                            Text(
+                              '${data.occupied}',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[800],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
 
-        //  return Card(
-        //       child: ListTile(
-        //         title: Text('Date and Time: ${data.dateAndTime}'),
-        //         subtitle: Column(
-        //           crossAxisAlignment: CrossAxisAlignment.start,
-        //           children: [
-        //             Text('Total: ${data.total}'),
-        //             Text('Occupied: ${data.occupied}'),
-        //           ],
-        //         ),
-        //       ),
-        //     );
-      },
-    ),
+                //  return Card(
+                //       child: ListTile(
+                //         title: Text('Date and Time: ${data.dateAndTime}'),
+                //         subtitle: Column(
+                //           crossAxisAlignment: CrossAxisAlignment.start,
+                //           children: [
+                //             Text('Total: ${data.total}'),
+                //             Text('Occupied: ${data.occupied}'),
+                //           ],
+                //         ),
+                //       ),
+                //     );
+              },
+            ),
 
-        // body: ListView.builder(
-        //   itemCount: _dataList.length,
-        //   itemBuilder: (context, index) {
-        //     final data = _dataList[index];
-        //     return Card(
-        //       child: ListTile(
-        //         title: Text('Date and Time: ${data.dateAndTime}'),
-        //         subtitle: Column(
-        //           crossAxisAlignment: CrossAxisAlignment.start,
-        //           children: [
-        //             Text('Total: ${data.total}'),
-        //             Text('Occupied: ${data.occupied}'),
-        //           ],
-        //         ),
-        //       ),
-        //     );
-        //   },
-        // ),
-      );
-    
+      // body: ListView.builder(
+      //   itemCount: _dataList.length,
+      //   itemBuilder: (context, index) {
+      //     final data = _dataList[index];
+      //     return Card(
+      //       child: ListTile(
+      //         title: Text('Date and Time: ${data.dateAndTime}'),
+      //         subtitle: Column(
+      //           crossAxisAlignment: CrossAxisAlignment.start,
+      //           children: [
+      //             Text('Total: ${data.total}'),
+      //             Text('Occupied: ${data.occupied}'),
+      //           ],
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
+    );
   }
 }
+
 // Center(child: Text(stringResponse.toString())),
- class Data {
+class Data {
   final DateTime dateAndTime;
   final int total;
   final int occupied;
 
-  Data({required this.dateAndTime, required this.total, required this.occupied});
+  Data(
+      {required this.dateAndTime, required this.total, required this.occupied});
 
   factory Data.fromJson(Map<String, dynamic> json) {
     return Data(
