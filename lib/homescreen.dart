cad175c872ac8b44647f3752cdd69574d3aa5c59
shreyash112002 <shreyashypatil1122002@gmail.com>
2023-04-26 00:02:10 +0530

    new url

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -34,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
   //     });
   //   }
   // }
-  final Url="https://script.googleusercontent.com/macros/echo?user_content_key=Ff4PESC-iXtVr9OsRg5R9d1pNDQi9pJMLJ8DO0RE6mgc2i4mN9kSvbTmVzVREB9urZWFTJxJzrewpOuxQTNZqGvuTngthLHtm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPveZDlJ_AsjVJCY6axLhPTyT107DOA6BeV8lpw_FsjPFjpp8noYC4ljtLZPJIzijjJM0SxjCYprq9OmLICGGxEjzvIRtMuI3tz9Jw9Md8uu&lib=MxUoWSxTNx4Hk4n3l8SvlMA9o7N4CQEWp";
+  final Url="https://script.googleusercontent.com/macros/echo?user_content_key=CnyZzTz7l7bXfFX1VRg61md9UX1H3oR8DQ7uzJ0uDnAZm6Ey2y9WA24ip9WMxnRAQVL9Baf3UMGAdEvxAb9SRglPL04gcRMfm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnF4W0jjeCGoVqtV2SVh4_DWyutOJbIG1XY5GbjlsRWc-l007Hrvmdhz_sbxKREBxpH6V-vOv-Zl3j7IRKlfVv-om28kck3SSM9z9Jw9Md8uu&lib=MxUoWSxTNx4Hk4n3l8SvlMA9o7N4CQEWp";
   List<Data> _dataList = [];
   bool _isLoading = true;
   Future<void> _getData() async {
